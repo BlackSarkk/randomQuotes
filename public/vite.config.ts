70956d821d9ca92ec9ac2764bccb74f7214cc5a7
BlackSarkk <droncer22446688@gmail.com>
2025-09-07 17:48:37 +0530

    feat: restore README.md and update vite.config.ts to load environment variables

--- public/vite.config.ts
+++ public/vite.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:5050",
+export default defineConfig(({ mode }) => {
+  // Load env variables for the current mode
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      allowedHosts: ['quotes.obscuredev.space'],
+      proxy: {
+        '/api': {
+          target: env.VITE_REACT_APP_BACKEND_BASEURL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+      host: true,
+      port: 5173,
     },
-    host: true,
-    port: 5173,
-  },
+  };
 });
